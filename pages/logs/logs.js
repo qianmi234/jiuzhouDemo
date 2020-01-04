ee4f9e6e383dd13f795025bcc6b71e2c5bf237c3
qianmi <qianmi234@163.com>
2020-01-04 09:23:36 +0800

    增加登录页面

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,27 @@
 //logs.js
 const util = require('../../utils/util.js')
-
+const app = getApp()
 Page({
   data: {
-    logs: []
+    nvabarData: {
+      showCapsule: 1, //是否显示左上角图标  1表示显示  0表示不显示
+      title: '添加银行卡', //导航栏 中间的标题
+      white: true, // 是就显示白的，不是就显示黑的。
+      address: '' // 加个背景 不加就是没有
+    },
+    height: app.globalData.height * 2 + 30,
+    logIcon:'../../images/login_logo@2x.png'
   },
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
       })
+    })
+  },
+  loginSubmit:function(){
+    wx.navigateTo({
+      url: '../index/index'
     })
   }
 })
